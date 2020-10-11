8b3704f6401930ef5fbcf51cb01ae1071ed641f5
Rohan <rohanrajput16.phy@gmail.com>
2020-10-11 17:10:50 +0530

    login and signup working

--- src/components/_pages/Login.js
+++ src/components/_pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 //@material ui components
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid';
@@ -6,7 +6,8 @@ import TextField from '@material-ui/core/TextField'
 import Container from "@material-ui/core/Container";
 import { Link, makeStyles, Typography } from '@material-ui/core';
 import { InputSharp } from '@material-ui/icons';
-
+import { useHistory } from 'react-router-dom';
+import M from "materialize-css";
 const useStyle = makeStyles({
     root:{
             textAlign:"center",
@@ -33,15 +34,58 @@ const useStyle = makeStyles({
     }
 })
 export default function Login(){
+    const [email, setEmail] = useState("");
+    const [password , setPassowrd] = useState("");
     const classes = useStyle();
+    const history = useHistory();
+    const postLogin = ()=>{
+        fetch("/login",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                email:email,
+                password:password
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            if(data.error){
+               M.toast({html: data.error , classes:"#c62828 red darken-3"});
+            }
+            else{
+                console.log(data)
+                M.toast({html: "Login Sucessfully" ,classes:"#2e7d32 green darken-3"})
+                history.push('/');
+            }
+        }).catch(err=>{
+            console.log(err);
+        })
+    }
     return(
         <Container className={classes.box} >
         <h1 className="font">Instagram</h1>
         <form>
         <Container maxWidth="xs" className={classes.root}>
-            <TextField id="outlined-basic" label="Username" variant="outlined" style={{width:"22vw", margin:"8px"}}/>
-            <TextField id="outlined-basic" label="Password" variant="outlined" style={{width:"22vw",margin:"8px"}} />
-            <Button variant="contained" color="primary" style={{width:"22vw",margin:"8px"}}>Login</Button>
+            <input
+             placeholder="Username"
+             value={email} 
+             onChange={(event)=>setEmail(event.target.value)}
+              style={{width:"22vw",
+               margin:"8px"}}/>
+
+            <input
+             placeholder="Password" 
+             value={password} 
+             onChange={(event)=>setPassowrd(event.target.value)}
+              style={{width:"22vw",margin:"8px"}} />
+
+            <Button variant="contained"
+             onClick={()=>postLogin()}
+             color="primary" 
+             style={{width:"22vw",margin:"8px"}}
+             
+             >Login</Button>
         </Container>
         </form>
         <Container>
--- src/components/_pages/SignUp.js
+++ src/components/_pages/SignUp.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import M from "materialize-css";
 //@material ui components
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid';
@@ -6,6 +7,7 @@ import TextField from '@material-ui/core/TextField'
 import Container from "@material-ui/core/Container";
 import { Link, makeStyles, Typography } from '@material-ui/core';
 import { InputSharp } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
 
 const useStyle = makeStyles({
     root:{
@@ -33,24 +35,35 @@ const useStyle = makeStyles({
     }
 })
 export default function SignUp(){
+    const history = useHistory();
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const postCredentials = ()=>{
+        if(!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
+            M.toast({html: "Inavlid Email" , classes:"#c62828 red darken-3"});
+            return
+        }
         fetch("/signup",{
                 method:"post",
                 headers:{
                 "Content-Type":"application/json"
              },
                 body:JSON.stringify({
-                    name:"",
-                    email:"",
-                    password:""
+                    name: name,
+                    email: email,
+                    password:password
                 })
             }).then(res => res.json())
             .then(data=>{
-                console.log(data); 
+                if(data.error){
+                    M.toast({html: data.error , classes:"#c62828 red darken-3"});
+                }
+                else{
+                    M.toast({html: data.message ,classes:"#2e7d32 green darken-3"})
+                    history.push("/login");
+                }
         }).catch(err=>{
             console.log(err);
         })
@@ -74,29 +87,30 @@ export default function SignUp(){
             
                /> */}
 
-            <TextField
+            <input
                 id="outlined-basic"
                 label="Full Name" 
-                variant="outlined" 
-                value={name}
+                placeholder="Full Name"
                 onChange={(event)=>setName(event.target.value)}
+                value={name}
                 style={{width:"22vw", 
                 margin:"6px"}}/>
 
-            <TextField 
+            <input 
                 id="outlined-basic"
                 label="Email" 
-                variant="outlined"
-                value={email}
                 onChange={(event)=>setEmail(event.target.value)}
+                placeholder="Email"
+                value={email}
                 style={{width:"22vw",
                 margin:"6px"}}/>
 
-            <TextField id="outlined-basic" 
+            <input id="outlined-basic" 
                 label="Password" 
                 type="password"
-                value={password}
+                placeholder="Password"
                 onChange={(event)=>setPassword(event.target.value)}
+                value={password}
                 variant="outlined"
                 style={{width:"22vw",margin:"6px"}} />
 
